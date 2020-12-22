27940393f302096c10e0c0bfc374bf8434a3e90f
Jose Guillermo Perez <jperez.dextersoft@gmail.com>
2020-12-22 08:13:41 -0300

    Ejecricio Consola Culminado

---
+++ src/test/java/com/dexterbits/DemoConsolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dexterbits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConsolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
